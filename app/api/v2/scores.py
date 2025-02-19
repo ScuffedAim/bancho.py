7c2b13f3821d6ed8f797bd90b91a85eff6b238ae
SneznyKocur <111371401+SneznyKocur@users.noreply.github.com>
2025-02-19 23:07:25 +0100

    deduplicate again

--- app/api/v2/scores.py
+++ app/api/v2/scores.py
@@ -100,7 +100,7 @@ async def get_matches(
         status=status,
         mode=mode,
         user_id=user_id,)
-    response = [MatchScore.from_mapping(rec).match_id for rec in data if MatchScore.from_mapping(rec).match_id not in response]
+    response = list(set([MatchScore.from_mapping(rec).match_id for rec in data]))
 
     return responses.success(
         content=response,
